b3d849cfbdfc61e417635497f3c82dedf9cb7260
maria habiba <38461475+mariahabiba078@users.noreply.github.com>
2020-08-27 02:06:28 +0600

    Update Armstrong number chk.py

--- Armstrong number chk.py
+++ Armstrong number chk.py
@@ -1,3 +1,7 @@
+#A positive integer is called an Armstrong number of order n if abcd... = an+bn+cn+dn+... in case of an Armstrong number of 3 digits,the sum of the cubes of each digit is equal to the number itself
+
+
+
 num = int(input("Enter the number to check:"))
 sum = 0
 temp = num
